a40a7f9e5bd45a60c7c5ce1ad9b79884fe0f5195
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-18 00:15:59 +0530

    setting class, style attributes and rendering list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,27 @@ import React, {Component} from "react";
 class Counter extends Component{
 	state={
 	count:0,
-	tags:['tag1','tag2','tag3']
+	tags:['tags1']
 	};
+	renderTags()
+	{
+	if (this.state.tags.length===0) return <p>There are no tags!</p>;
+
+	return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>;
+	}
+	handleIncrement()
+	{
+	console.log('Increment clicked');
+	}
 	render()
 	{
 	 return (
 	<React.Fragment>
 	
-	 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><button className="btn btn-secondary btn-sm">Increment</button>
-	 <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
+	 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+{this.state.tags.length===0 && 'plz create a new string'}
+	 {this.renderTags()}
+	 
 	 </React.Fragment>
 	 );
 	}
