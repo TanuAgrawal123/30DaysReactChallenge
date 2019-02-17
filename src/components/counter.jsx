2fef3e7285f11bc56116d92e2b0c92c7263bae84
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-17 23:49:14 +0530

    setting class, style attributes and rendering list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,16 @@ import React, {Component} from "react";
 
 class Counter extends Component{
 	state={
-	count:1,
-	imageUrl:'https://picsum.photos/200'};
+	count:0,
+	tags:['tag1','tag2','tag3']
+	};
 	render()
 	{
 	 return (
 	<React.Fragment>
-	<img src={this.state.imageUrl}/>
-	 <span>{this.formatCount()}</span><button>Increment</button>
+	
+	 <span className={this.getBadgeClasses()}>{this.formatCount()}</span><button className="btn btn-secondary btn-sm">Increment</button>
+	 <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
 	 </React.Fragment>
 	 );
 	}
@@ -17,6 +19,10 @@ class Counter extends Component{
 	const {count}=this.state;
 	return count===0 ? "Zero" : count;
 	}
+	getBadgeClasses()
+	{let classes="badge m-2 badge-";
+	classes+=this.state.count===0? "warning" : "primary";
+	return classes}
 	}
 
 	
