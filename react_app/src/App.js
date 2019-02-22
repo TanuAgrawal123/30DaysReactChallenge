27d4702adfe39703347175f09885c61f6c617dd7
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-22 11:56:49 +0530

    State

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,11 +4,20 @@ import TodoItem from './components/todoitem'
 import TodoData from './components/tododata'
 import './css/style.css'
 
-function App()
-{const todocomponents=TodoData.map(item => <TodoItem key={item.id} items={item} />)
-	return (
-		<div>
-		{todocomponents}
-	</div>)
-}
+class App extends React.Component{
+	constructor()
+	{super()
+		this.state={name:"tanu",
+		age:20}
+	}
+	render()
+	{
+		return (
+			<div>
+			<h1>{this.state.name}</h1>
+			<h2>{this.state.age} years old</h2>
+			</div>
+		)
+}}
+
  export default App
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,50 +1,6 @@
 import React, {Component} from 'react'
 import ReactDOM from "react-dom"
-
-
-class App extends React.Component{
-	render()
-	{
-		return(
-			<div>
-			<Header username="tanuagrawal" />
-			<Greating />
-			</div>
-			)
-	}
-}
-
-class Header extends Component{
-	render()
-	{
-		return(
-		<header>
-		<p>Welcome!{this.props.username}</p>
-		</header>
-		)
-	}
-}
-
-
-class Greating extends React.Component{
-	render()
-	{
-		const date=new Date()
-		const hours= date.getHours()
-		let timeOfDay
-		if(hours<12)
-		{
-			timeOfDay="morning"
-		}
-		else if(hours>=12 && hours<17)
-			timeOfDay="afternoon"
-		else
-			timeOfDay="night"
-	
-	return(
-	<h2>Good {timeOfDay} to you!..sir or madam</h2>)
-
-}}
+import App from'./App.js'
 
 
 
