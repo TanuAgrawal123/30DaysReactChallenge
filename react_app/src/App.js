b366fa5e2129aa74689de7b8e37fa62e2f8b920a
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-22 12:50:51 +0530

    event handling and changing state

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,28 +3,49 @@ import React from 'react'
 import TodoItem from './components/todoitem'
 import TodoData from './components/tododata'
 import './css/style.css'
-
+/*
 class App extends React.Component{
 	constructor()
 	{super()
-		this.state={isLogedin:false
-			}
-
+		this.state={
+			todos:TodoData
+		}
+	}
+	render()
+	{	
+const todocomponents=this.state.todos.map(item => <TodoItem key={item.id} items={item} />)
+	return (
+		<div>
+		{todocomponents}
+	</div>)
+}
+}
+*/
+class App extends React.Component{
+	constructor()
+	{
+		super()
+		this.state={
+			count:0
+		}
+		this.handleclick=this.handleclick.bind(this)
 
 		}
-	
-	render()
-	{let wordDisplay
-		if (!this.state.isLogedin)
-			wordDisplay="out"
-		else
-			wordDisplay="in"
-		return (
-			<div>
-			<h1>You Are Currently loged {wordDisplay}</h1>
-			
-			</div>
-		)
-}}
+		handleclick()
 
+		{
+			this.setState(prevState => {return {count:prevState.count +1}})
+		}
+		render(){
+			
+			return(
+				<div>
+				<h1>{this.state.count}</h1>
+				<button onClick={this.handleclick}>Change!</button>
+
+				</div>
+				)
+		}
+	}
  export default App
+
--- react_app/src/components/todoitem.js
+++ react_app/src/components/todoitem.js
@@ -4,7 +4,7 @@ import React from 'react'
 function TodoItem(props){
 	return(
 <div className="todo_item">
-<input type="checkbox" checked={props.items.completed} />
+<input type="checkbox" checked={props.items.completed} onChange={() => console.log("changed")} />
 <h2>{props.items.text}</h2>
 <hr/>
 
